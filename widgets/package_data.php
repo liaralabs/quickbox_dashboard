5f6ded8ea2934e6e4fdd435a34a0b2a980eb93e7
JMSDOnline <jmsolodesigns@gmail.com>
2016-06-05 14:56:01 -0500

    new system output updates for packages - now reflecting actual bash output

--- widgets/package_data.php
+++ widgets/package_data.php
@@ -1,108 +1,48 @@
 <?php
 
 if (isset($_GET['installpackage-btsync'])) {
-        //header('Location: /');
-        header('Refresh: 15; /');
-        writeMsg("Hello <b>$username</b>: I am completing the installation of the <b>BTSync</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 15 seconds ... <br>");
-        $message = "Hello <b>$username</b>: I am completing the installation of the <b>BTSync</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 15 seconds ... <br><br>";
+        header('Refresh: 2; /');
         shell_exec("sudo /usr/local/bin/quickbox/package/install/installpackage-btsync");
-        $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
-        $myUrl .= '://'.$_SERVER['HTTP_HOST'];
-        $newURL = $myURL;
 }
 if (isset($_GET['removepackage-btsync'])) {
-        //header('Location: /');
-        header('Refresh: 15; /');
-        writeMsg("Hello <b>$username</b>: I am completing the removal of the <b>BTSync</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 15 seconds ... <br>");
-        $message = "Hello <b>$username</b>: I am completing the removal of the <b>BTSync</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 15 seconds ... <br>";
+        header('Refresh: 2; /');
         shell_exec("sudo /usr/local/bin/quickbox/package/remove/removepackage-btsync");
-        $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
-        $myUrl .= '://'.$_SERVER['HTTP_HOST'];
-        $newURL = $myURL;
 }
 
 if (isset($_GET['installpackage-csf'])) {
-        //header('Location: /');
-        header('Refresh: 15; /');
-        writeMsg("Hello <b>$username</b>: I am completing the installation of the <b>Config Server Firewall (CSF)</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 15 seconds ... <br>");
-        $message = "Hello <b>$username</b>: I am completing the installation of the <b>Config Server Firewall (CSF)</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 15 seconds ... <br><br>";
+        header('Refresh: 2; /');
         shell_exec("sudo /usr/local/bin/quickbox/package/install/installpackage-csf");
-        $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
-        $myUrl .= '://'.$_SERVER['HTTP_HOST'];
-        $newURL = $myURL;
 }
 if (isset($_GET['removepackage-csf'])) {
-        //header('Location: /');
-        header('Refresh: 15; /');
-        writeMsg("Hello <b>$username</b>: I am completing the removal of the <b>Config Server Firewall (CSF)</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 15 seconds ... <br>");
-        $message = "Hello <b>$username</b>: I am completing the removal of the <b>Config Server Firewall (CSF)</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 15 seconds ... <br>";
+        header('Refresh: 2; /');
         shell_exec("sudo /usr/local/bin/quickbox/package/remove/removepackage-csf");
-        $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
-        $myUrl .= '://'.$_SERVER['HTTP_HOST'];
-        $newURL = $myURL;
 }
 
 if (isset($_GET['installpackage-plex'])) {
-        //header('Location: /');
-        header('Refresh: 15; /');
-        writeMsg("Hello <b>$username</b>: I am completing the installation of the <b>Plex Media Server</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 15 seconds ... <br>");
-        $message = "Hello <b>$username</b>: I am completing the installation of the <b>Plex Media Server</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 15 seconds ... <br>";
+        header('Refresh: 1; /');
         shell_exec("sudo /usr/local/bin/quickbox/package/install/installpackage-plex");
-        $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
-        $myUrl .= '://'.$_SERVER['HTTP_HOST'];
-        $newURL = $myURL;
 }
 if(isset($_GET['removepackage-plex'])) {
-        //header('Location: /');
-        header('Refresh: 15; /');
-        writeMsg("Hello <b>$username</b>: I am completing the removal of the <b>Plex Media Server</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 15 seconds ... <br>");
-        $message = "Hello <b>$username</b>: I am completing the removal of the <b>Plex Media Server</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 15 seconds ... <br>";
+        header('Refresh: 1; /');
         shell_exec("sudo /usr/local/bin/quickbox/package/remove/removepackage-plex");
-        $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
-        $myUrl .= '://'.$_SERVER['HTTP_HOST'];
-        $newURL = $myURL;
 }
 
 if (isset($_GET['installpackage-rapidleech'])) {
-        //header('Location: /');
-        header('Refresh: 15; /');
-        writeMsg("Hello <b>$username</b>: I am completing the installation of the <b>Rapidleech</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 15 seconds ... <br>");
-        $message = "Hello <b>$username</b>: I am completing the installation of the <b>Rapidleech</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 15 seconds ... <br>";
+        header('Refresh: 2; /');
         shell_exec("sudo /usr/local/bin/quickbox/package/install/installpackage-rapidleech");
-        $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
-        $myUrl .= '://'.$_SERVER['HTTP_HOST'];
-        $newURL = $myURL;
 }
 if (isset($_GET['removepackage-rapidleech'])) {
-        //header('Location: /');
-        header('Refresh: 15; /');
-        writeMsg("Hello <b>$username</b>: I am completing the removal of the <b>Rapidleech</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 15 seconds ... <br>");
-        $message = "Hello <b>$username</b>: I am completing the removal of the <b>Rapidleech</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 15 seconds ... <br>";
+        header('Refresh: 2; /');
         shell_exec("sudo /usr/local/bin/quickbox/package/remove/removepackage-rapidleech");
-        $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
-        $myUrl .= '://'.$_SERVER['HTTP_HOST'];
-        $newURL = $myURL;
 }
 
 if (isset($_GET['installpackage-sickrage'])) {
-        //header('Location: /');
-        header('Refresh: 15; /');
-        writeMsg("Hello <b>$username</b>: I am completing the installation of the <b>SickRage</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 15 seconds ... <br>");
-        $message = "Hello <b>$username</b>: I am completing the installation of the <b>SickRage</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 15 seconds ... <br>";
+        header('Refresh: 2; /');
         shell_exec("sudo /usr/local/bin/quickbox/package/install/installpackage-sickrage");
-        $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
-        $myUrl .= '://'.$_SERVER['HTTP_HOST'];
-        $newURL = $myURL;
 }
 if (isset($_GET['removepackage-sickrage'])) {
-        //header('Location: /');
-        header('Refresh: 15; /');
-        writeMsg("Hello <b>$username</b>: I am completing the removal of the <b>SickRage</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 15 seconds ... <br>");
-        $message = "Hello <b>$username</b>: I am completing the removal of the <b>SickRage</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 15 seconds ... <br>";
+        header('Refresh: 2; /');
         shell_exec("sudo /usr/local/bin/quickbox/package/remove/removepackage-sickrage");
-        $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
-        $myUrl .= '://'.$_SERVER['HTTP_HOST'];
-        $newURL = $myURL;
 }
 
 ?>

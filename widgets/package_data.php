21efa1f0e89d6b9b6ec8499feba2da5775367303
JMSDOnline <jmsolodesigns@gmail.com>
2016-06-06 11:52:04 -0500

    [fix] updated header info to reutn a refresh after install only once

--- widgets/package_data.php
+++ widgets/package_data.php
@@ -1,47 +1,47 @@
 <?php
 
 if (isset($_GET['installpackage-btsync'])) {
-        header('Refresh: 2; /');
+        header('Location: /');
         shell_exec("sudo /usr/local/bin/quickbox/package/install/installpackage-btsync");
 }
 if (isset($_GET['removepackage-btsync'])) {
-        header('Refresh: 2; /');
+        header('Location: /');
         shell_exec("sudo /usr/local/bin/quickbox/package/remove/removepackage-btsync");
 }
 
 if (isset($_GET['installpackage-csf'])) {
-        header('Refresh: 2; /');
+        header('Location: /');
         shell_exec("sudo /usr/local/bin/quickbox/package/install/installpackage-csf");
 }
 if (isset($_GET['removepackage-csf'])) {
-        header('Refresh: 2; /');
+        header('Location: /');
         shell_exec("sudo /usr/local/bin/quickbox/package/remove/removepackage-csf");
 }
 
 if (isset($_GET['installpackage-plex'])) {
-        header('Refresh: 1; /');
+        header('Location: /');
         shell_exec("sudo /usr/local/bin/quickbox/package/install/installpackage-plex");
 }
 if(isset($_GET['removepackage-plex'])) {
-        header('Refresh: 1; /');
+        header('Location: /');
         shell_exec("sudo /usr/local/bin/quickbox/package/remove/removepackage-plex");
 }
 
 if (isset($_GET['installpackage-rapidleech'])) {
-        header('Refresh: 2; /');
+        header('Location: /');
         shell_exec("sudo /usr/local/bin/quickbox/package/install/installpackage-rapidleech");
 }
 if (isset($_GET['removepackage-rapidleech'])) {
-        header('Refresh: 2; /');
+        header('Location: /');
         shell_exec("sudo /usr/local/bin/quickbox/package/remove/removepackage-rapidleech");
 }
 
 if (isset($_GET['installpackage-sickrage'])) {
-        header('Refresh: 2; /');
+        header('Location: /');
         shell_exec("sudo /usr/local/bin/quickbox/package/install/installpackage-sickrage");
 }
 if (isset($_GET['removepackage-sickrage'])) {
-        header('Refresh: 2; /');
+        header('Location: /');
         shell_exec("sudo /usr/local/bin/quickbox/package/remove/removepackage-sickrage");
 }
 

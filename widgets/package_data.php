d9aeb3eaabd47911bc42d1bb40fa5ed8959e4400
JMSDOnline <jmsolodesigns@gmail.com>
2016-06-06 14:55:41 -0500

    [enhancement] making use of arrays for easier package and plugin data viewing
    Tired of 1,000 lines of data code for widgets and especially plugins? Me too.

--- widgets/package_data.php
+++ widgets/package_data.php
@@ -1,48 +1,23 @@
 <?php
 
-if (isset($_GET['installpackage-btsync'])) {
-        header('Location: /');
-        shell_exec("sudo /usr/local/bin/quickbox/package/install/installpackage-btsync");
-}
-if (isset($_GET['removepackage-btsync'])) {
-        header('Location: /');
-        shell_exec("sudo /usr/local/bin/quickbox/package/remove/removepackage-btsync");
-}
+$packages = array(
+        'btsync',
+        'csf',
+        'plex',
+        'rapidleech',
+        'sickrage'
+);
 
-if (isset($_GET['installpackage-csf'])) {
+foreach ($packages as $package) {
+if (isset($_GET['installpackage-'.$package.''])) {
         header('Location: /');
-        shell_exec("sudo /usr/local/bin/quickbox/package/install/installpackage-csf");
-}
-if (isset($_GET['removepackage-csf'])) {
-        header('Location: /');
-        shell_exec("sudo /usr/local/bin/quickbox/package/remove/removepackage-csf");
-}
+        shell_exec("sudo /usr/local/bin/quickbox/package/install/installpackage-$package");
+}}
 
-if (isset($_GET['installpackage-plex'])) {
+foreach ($packages as $package) {
+if (isset($_GET['removepackage-'.$package.''])) {
         header('Location: /');
-        shell_exec("sudo /usr/local/bin/quickbox/package/install/installpackage-plex");
-}
-if(isset($_GET['removepackage-plex'])) {
-        header('Location: /');
-        shell_exec("sudo /usr/local/bin/quickbox/package/remove/removepackage-plex");
-}
-
-if (isset($_GET['installpackage-rapidleech'])) {
-        header('Location: /');
-        shell_exec("sudo /usr/local/bin/quickbox/package/install/installpackage-rapidleech");
-}
-if (isset($_GET['removepackage-rapidleech'])) {
-        header('Location: /');
-        shell_exec("sudo /usr/local/bin/quickbox/package/remove/removepackage-rapidleech");
-}
-
-if (isset($_GET['installpackage-sickrage'])) {
-        header('Location: /');
-        shell_exec("sudo /usr/local/bin/quickbox/package/install/installpackage-sickrage");
-}
-if (isset($_GET['removepackage-sickrage'])) {
-        header('Location: /');
-        shell_exec("sudo /usr/local/bin/quickbox/package/remove/removepackage-sickrage");
-}
+        shell_exec("sudo /usr/local/bin/quickbox/package/remove/removepackage-$package");
+}}
 
 ?>

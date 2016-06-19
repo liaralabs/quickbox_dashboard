3bf97e71be3f9aa5e3deeae6ab2b2726964191b7
JMSDOnline <jmsolodesigns@gmail.com>
2016-06-19 10:30:54 -0500

    version bump

--- inc/config.php
+++ inc/config.php
@@ -2,7 +2,7 @@
 session_destroy();
 include '/srv/rutorrent/php/util.php';
 include 'widgets/class.php';
-$version = "v2.3.5";
+$version = "v2.3.6";
 error_reporting(E_ALL);
 $master = file_get_contents('/srv/rutorrent/home/db/master.txt');
 $username = getUser();

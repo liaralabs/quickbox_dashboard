ad277cef2fecbb1b7252ae9493cfc06b9099b149
JMSDOnline <jmsolodesigns@gmail.com>
2016-06-21 14:33:43 -0500

    cleaned out forward slashes inside php code block ;)

--- inc/panel.menu.php
+++ inc/panel.menu.php
@@ -138,9 +138,9 @@
                 <?php } ?>
                 </li>
                 <?php if (file_exists("/install/.x2go.lock")) { ?>
-                  <a href=\"javascript:void()\" data-toggle=\"modal\" data-target=\"#x2goRemovalConfirm\">x2go  <span class=\"pull-right plgin-center-switch\"><img src=\"img/switch-installed.png\"></span></a>";
+                  <a href="javascript:void()" data-toggle="modal" data-target="#x2goRemovalConfirm">x2go  <span class="pull-right plgin-center-switch"><img src="img/switch-installed.png"></span></a>";
                 <?php } else { ?>
-                  <a href=\"?installpackage-x2go=true\" data-toggle=\"modal\" data-target=\"#sysResponse\" id=\"x2goInstall\">x2go  <span class=\"pull-right plgin-center-switch\"><img src=\"img/switch-notinstalled.png\"></span></a>";
+                  <a href="?installpackage-x2go=true" data-toggle="modal" data-target="#sysResponse" id="x2goInstall">x2go  <span class="pull-right plgin-center-switch"><img src="img/switch-notinstalled.png"></span></a>";
                 <?php } ?>
                 echo "</li>";
               </ul>

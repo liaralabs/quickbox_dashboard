2f52278307011be48f5aaf37b5ba45107dec65e5
niayh <samillerapp@gmail.com>
2016-06-27 15:51:26 -0700

    Add necessary commands (if) for toggles

--- inc/config.php
+++ inc/config.php
@@ -335,8 +335,13 @@ case 66:
       } else {
         $output = substr($thisname, 0, strpos(strtolower($thisname), '_')); $servicename = strtolower($output);
       }
-    shell_exec("printf '%s\n' ',s/$thisname/$name/g' wq | ed -s /home/$username/.startup");
-    $output = substr($thisname, 0, strpos(strtolower($thisname), '_'));
+      if (file_exists('/install/.cron.lock')) {
+        shell_exec("printf '%s\n' ',s/$thisname/$name/g' wq | ed -s /home/$username/.startup");
+        $output = substr($thisname, 0, strpos(strtolower($thisname), '_'));
+      } else {
+        shell_exec("sudo systemctl enable $servicename@$username");
+        shell_exec("sudo systemctl start $servicename@$username");
+      }
     } else {}
   header('Location: https://' . $_SERVER['HTTP_HOST'] . '/');
 break;
@@ -356,8 +361,14 @@ case 77:
         $output = substr($thisname, 0, strpos(strtolower($thisname), '_')); $servicename = strtolower($output);
         if (strpos($servicename,'rtorrent') !== false) { $servicename="main"; }
       }
-      shell_exec("printf '%s\n' ',s/$name/$thisname/g' wq | ed -s /home/$username/.startup");
-      shell_exec("sudo -u $username pkill -9 $servicename");
+      if (file_exists('/install/.cron.lock')) {
+        shell_exec("printf '%s\n' ',s/$name/$thisname/g' wq | ed -s /home/$username/.startup");
+        shell_exec("sudo -u $username pkill -9 $servicename");
+      } else {
+        shell_exec("sudo systemctl stop $servicename@$username");
+        shell_exec("sudo systemctl disable $servicename@$username");
+      }
+
     } else {}
   header('Location: https://' . $_SERVER['HTTP_HOST'] . '/');
 break;

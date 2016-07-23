fbbed04ea928934b15af70dbb0464858385984e8
JMSolo <jmsolodesigns@gmail.com>
2016-07-23 11:42:03 -0500

    [fix] uptime translations fixed

--- widgets/up.php
+++ widgets/up.php
@@ -1,7 +1,5 @@
 <?php
-
 include ("../inc/localize.php");
-
   header('Refresh: 1');
   header('Content-Type: text/event-stream');
   header('Cache-Control: no-cache');
@@ -10,5 +8,6 @@ include ("../inc/localize.php");
   $hours = $uptime/60/60%24;
   $mins = $uptime/60%60;
   $secs = $uptime%60;
-  echo "<span style=\"font-size:14px;color:#dedede;\"><b>$days</b> <small>T('DAYS_L')</small>  <b>$hours</b> <small>T('HOURS_L')</small>  <b>$mins</b> <small>T('MINUTES_L')</small>  <b>$secs</b> <small>T('SECONDS_L')</small></span> ";
-flush();
+?>
+  <span style="font-size:14px;color:#dedede;"><b><?php echo "$days" ?></b> <small><?php echo T('DAYS_L'); ?></small>  <b><?php echo "$hours" ?></b> <small><?php echo T('HOURS_L'); ?></small>  <b><?php echo "$mins" ?></b> <small><?php echo T('MINUTES_L'); ?></small>  <b><?php echo "$secs" ?></b> <small><?php echo T('SECONDS_L'); ?></small></span>
+<?php flush(); ?>

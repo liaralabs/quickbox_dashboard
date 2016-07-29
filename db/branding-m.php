a2db8e70d60defd832fc98f685fefdb5ae1c4ff4
JMSolo <jmsolodesigns@gmail.com>
2016-07-29 12:35:55 -0500

    cleanup unused code blocks for way of **new** update widget

--- db/branding-m.php
+++ db/branding-m.php
@@ -2,10 +2,5 @@
   <li><span style="margin-top:5x"></span></li>
   <li><a href="https://plaza.quickbox.io/c/quickbox-support" target="_blank"><i class="fa fa-warning text-warning"></i> Report an issue </a></li>
   <li><a href="https://quickbox.io/donate" target="_blank"><i class="fa fa-heart text-danger"></i> Donate </a></li>
-  <div class="usermenu-div"></div>
-  <li style="border-bottom:1px solid #3a3a3a"><pre style="font-size:10px; color: #aaa; padding-bottom:4px">QuickBox Version :: <?php echo "$version"; ?></pre></li>
-  <?php if ($username == "$master") { ?>
-    <li><a href="?updateQuickBox=true" data-toggle="modal" data-target="#sysResponse" style=""><i class="fa fa-bell text-success"></i> Check for updates </a></li>
-  <?php } ?>
   </span></li>
 </ul>

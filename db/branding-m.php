771e5539253137e0e1d37a67b4dbd222c36ed093
JMSolo <jmsolodesigns@gmail.com>
2016-08-02 16:57:50 -0500

    leftover code lines

--- db/branding-m.php
+++ db/branding-m.php
@@ -2,5 +2,4 @@
   <li><span style="margin-top:5x"></span></li>
   <li><a href="https://plaza.quickbox.io/c/quickbox-support" target="_blank"><i class="fa fa-warning text-warning"></i> Report an issue </a></li>
   <li><a href="https://quickbox.io/donate" target="_blank"><i class="fa fa-heart text-danger"></i> Donate </a></li>
-  </span></li>
 </ul>

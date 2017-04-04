895cfb7ee7e71611e4ceed155200ad256e6a4500
JMSolo <jmsolodesigns@gmail.com>
2017-04-04 16:07:15 -0500

    update chat link back to chat server

--- inc/panel.menu.php
+++ inc/panel.menu.php
@@ -45,11 +45,11 @@
                     <span class="title" style="color: #fff; font-weight: 300; font-size: 13px;"><?php echo T('JOIN_US_TXT'); ?></span>
                     </li>
                     <li>
-                    <span style="padding: 6px 10px; color:#fff"><strong style="color: #4CD4B0">host:</strong> gitter.im/QuickBox</span><br/>
-                    <span style="padding: 6px 10px; color:#fff"><strong style="color: #4CD4B0">chan:</strong> #Support</span><br/>
+                    <span style="padding: 6px 10px; color:#fff"><strong style="color: #4CD4B0">host:</strong> chat.quickbox.io</span><br/>
+                    <span style="padding: 6px 10px; color:#fff"><strong style="color: #4CD4B0">chan:</strong> #QuickBox.io</span><br/>
                     </li>
                     <li style="border-top: 1px solid #444">
-                      <span class="chat-btn"><a href="https://gitter.im/QuickBox/Support" class="label label-success" target="_blank"><?php echo T('CHAT_CONNECT_TXT'); ?></a></span>
+                      <span class="chat-btn"><a href="https://chat.quickbox.io/channel/QuickBox.io" class="label label-success" target="_blank"><?php echo T('CHAT_CONNECT_TXT'); ?></a></span>
                     </li>
                 </ul>
               </div>

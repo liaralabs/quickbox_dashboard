0d339e067216ae5ad35fecad45937017ed369f52
liaralabs <liara@swizzin.ltd>
2017-07-13 12:14:47 -0400

    Update package locations for installers/uninstallers

--- widgets/package_data.php
+++ widgets/package_data.php
@@ -30,13 +30,13 @@ $packages = array(
 foreach ($packages as $ipackage) {
 if (isset($_GET['installpackage-'.$ipackage.''])) {
         header('Location: //');
-        shell_exec("sudo /usr/local/bin/quickbox/package/install/installpackage-$ipackage");
+        shell_exec("sudo /usr/local/bin/swizzin/install/$ipackage.sh");
 }}
 
 foreach ($packages as $rpackage) {
 if (isset($_GET['removepackage-'.$rpackage.''])) {
         header('Location: /');
-        shell_exec("sudo /usr/local/bin/quickbox/package/remove/removepackage-$rpackage");
+        shell_exec("sudo /usr/local/bin/swizzin/remove/$rpackage.sh");
 }}
 
 ?>
